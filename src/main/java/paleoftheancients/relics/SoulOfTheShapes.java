b80146a2f1f7bb35c757648e782992d44f6bf54c
Raz <razash@gmail.com>
2020-01-15 08:43:24 +0100

    Fixed shapes relic.

--- src/main/java/paleoftheancients/relics/SoulOfTheShapes.java
+++ src/main/java/paleoftheancients/relics/SoulOfTheShapes.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.LoseDexterityPower;
 import com.megacrit.cardcrawl.powers.LoseStrengthPower;
@@ -30,7 +31,7 @@ public class SoulOfTheShapes extends CustomRelic {
 
     @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0] + DESCRIPTIONS[mode == DonuDeca.Mode.Deca ? 1 : 2];
+        return DESCRIPTIONS[0] + DESCRIPTIONS[mode == DonuDeca.Mode.Donu ? 1 : 2];
     }
 
     @Override
@@ -41,7 +42,6 @@ public class SoulOfTheShapes extends CustomRelic {
     public void onUseCard(AbstractCard card, UseCardAction action) {
         if(mode == DonuDeca.Mode.Deca) {
             mode = DonuDeca.Mode.Donu;
-            setImage();
             if(card.type == AbstractCard.CardType.SKILL) {
                 this.flash();
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STRENGTH), STRENGTH));
@@ -49,19 +49,23 @@ public class SoulOfTheShapes extends CustomRelic {
             }
         } else {
             mode = DonuDeca.Mode.Deca;
-            setImage();
             if(card.type == AbstractCard.CardType.ATTACK) {
                 this.flash();
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new DexterityPower(AbstractDungeon.player, DEX), DEX));
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new LoseDexterityPower(AbstractDungeon.player, DEX), DEX));
             }
         }
+        setImage();
     }
 
     private void setImage() {
         String tmp = mode.name().toLowerCase();
         img = AssetLoader.loadImage(PaleMod.assetPath("images/relics/donudeca_" + tmp + ".png"));
         outlineImg = AssetLoader.loadImage(PaleMod.assetPath("images/relics/outline/donudeca_" + tmp + ".png"));
+        this.description = this.getUpdatedDescription();
+        this.tips.clear();
+        this.tips.add(new PowerTip(this.name, this.description));
+        this.initializeTips();
     }
 
     @Override
