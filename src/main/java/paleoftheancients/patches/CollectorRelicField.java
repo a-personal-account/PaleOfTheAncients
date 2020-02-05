d53653f7dbe12ef4113bb1f8a31133a340dbd9f4
Raz <razash@gmail.com>
2020-02-05 14:56:10 +0100

    Removed unnecessary patch. Added shapes flavortext.

--- src/main/java/paleoftheancients/patches/CollectorRelicField.java
+++ src/main/java/paleoftheancients/patches/CollectorRelicField.java
@@ -3,7 +3,6 @@ package paleoftheancients.patches;
 import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
 
 @SpirePatch(clz = AbstractCard.class, method = SpirePatch.CLASS)
 public class CollectorRelicField {
@@ -13,18 +12,6 @@ public class CollectorRelicField {
     public static class MakeStatEquivalentCopy {
         public static AbstractCard Postfix(AbstractCard result, AbstractCard self) {
             isBottled.set(result, isBottled.get(self));
-            return result;
-        }
-    }
-
-    @SpirePatch(clz = CardGroup.class, method = "getPurgeableCards")
-    public static class UnPurgeable {
-        public static CardGroup Postfix(CardGroup result, CardGroup __instance) {
-            for(int i = result.group.size() - 1; i >= 0; i--) {
-                if (isBottled.get(result.group.get(i))) {
-                    result.group.remove(i);
-                }
-            }
             return result;
         }
     }
